c2cfd67badf22e15ce8c9accc53ebd1294611426
qianjiangchao1992 <qjchao1992@163.com>
2018-08-03 14:28:40 +0800

    线性回归模型修改加入placeholder占位符

--- regression_01.py
+++ regression_01.py
@@ -7,11 +7,11 @@ Created on Thu Aug  2 13:31:36 2018
 #以线性回归为例，构建Tensorflow计算图
 import tensorflow as tf
 #第一步，给出X，Y
-x_train=tf.constant([1.,2.,3.])
-y_train=tf.constant([1.,2.,3.])
+x_train=tf.placeholder(tf.float32) #占位符一定要有数据类型
+y_train=tf.placeholder(tf.float32)
 #第二步设置变量，w,b
-w=tf.Variable(tf.random_normal([1]),name='weight')
-b=tf.Variable(tf.random_normal([1]),name='basis')
+w=tf.Variable(tf.random_normal([1]),name='weight') #变量一定要有初始化值
+b=tf.Variable(tf.random_normal([1]),name='basis')  #变量一定要有初始化值
 #给出假设函数
 hypothesis=w*x_train+b
 #cost=1/m*∑(hypothesis-y_train)^2
@@ -23,9 +23,9 @@ train=optimize.minimize(cost)#建立训练模型
 sess=tf.Session()  #开启会话
 sess.run(tf.global_variables_initializer()) #初始化所有变量
 for step in range(2001):
-    sess.run(train)   #开始训练模型
+    cost_val,w_val,b_val,_=sess.run([cost,w,b,train],feed_dict=
+                                    {x_train:[1,2,3],y_train:[6,7,8]})#开始训练模型
     if step%20==0:
-        print(step,sess.run(cost),sess.run(w),sess.run(b))
-write=tf.summary.FileWriter('regession_01_logs',sess.graph)
-write.close()
+        print(step,cost_val,w_val,b_val)
+
 
